a5b7b2d83cb624bd3184db062a315b9686de01f3
crtmachiny <crtmachiny@tf.otbebucb>
2026-06-10 23:44:59 +0300

    i just made some bullllllshittttt

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -4,30 +4,40 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from app.models.user import User
+from app.models import User
 
 
 class UserRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
+    async def get_all(self, skip: int, limit: int) -> Sequence[User]:
+        """Получить список пользователей с пагинацией"""
+
+        users = await self.session.scalars(select(User).offset(skip).limit(limit))
+        return users.all()
+
     async def get_all_with_division(self, skip: int, limit: int) -> Sequence[User]:
-        """Получить список пользователей с пагинацией и подразделениями."""
+        """Получить список пользователей с пагинацией и подразделениями"""
 
         users = await self.session.scalars(
             select(User).offset(skip).limit(limit).options(joinedload(User.division))
         )
         return users.all()
 
+    async def get_by_id(self, user_id: int) -> User | None:
+        """Получить одного пользователя по ID"""
+        return await self.session.scalar(select(User).where(User.id == user_id))
+
     async def get_by_id_with_division(self, user_id: int) -> User | None:
-        """Получить одного пользователя по ID со связанным подразделением."""
+        """Получить одного пользователя по ID со связанным подразделением"""
 
         return await self.session.scalar(
             select(User).where(User.id == user_id).options(joinedload(User.division))
         )
 
     async def create(self, **kwargs: Any) -> User:
-        """Добавить нового пользователя в сессию."""
+        """Добавить нового пользователя в сессию"""
 
         user = User(**kwargs)
         self.session.add(user)
@@ -35,7 +45,7 @@ class UserRepository:
         return cast(User, await self.get_by_id_with_division(user.id))
 
     async def update(self, user: User, **kwargs: Any) -> User:
-        """Обновить поля пользователя из переданных kwargs."""
+        """Обновить поля пользователя из переданных kwargs"""
 
         for key, value in kwargs.items():
             setattr(user, key, value)
@@ -43,7 +53,7 @@ class UserRepository:
         return cast(User, await self.get_by_id_with_division(user.id))
 
     async def delete(self, user: User) -> None:
-        """Пометить пользователя на удаление."""
+        """Пометить пользователя на удаление"""
 
         await self.session.delete(user)
         await self.session.flush()
